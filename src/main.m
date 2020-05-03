92c218aa935da09cc5c9292c9c50b28582c7c90b
Zeba Karishma <zebakarishma@gmail.com>
2020-05-03 16:36:54 -0400

    Driver file

---
+++ src/main.m
@@ -0,0 +1,7 @@
+%driver code to run all the code at once
+
+%call ransac to get the set of inliers point forming the dominant plane
+Ransac2()
+
+%code to take inliers and perform transformations
+transform()
